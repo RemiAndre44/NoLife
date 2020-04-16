8664a6c67c9831ba590602617466e147520dd5ce
Santi <remi44andre@gmail.com>
2020-04-16 18:05:28 +0200

    maj symfony

--- src/Repository/CommentLikeRepository.php
+++ src/Repository/CommentLikeRepository.php
@@ -4,7 +4,7 @@ namespace App\Repository;
 
 use App\Entity\CommentLike;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
-use Symfony\Bridge\Doctrine\RegistryInterface;
+use Doctrine\Persistence\ManagerRegistry;
 
 /**
  * @method CommentLike|null find($id, $lockMode = null, $lockVersion = null)
@@ -14,7 +14,7 @@ use Symfony\Bridge\Doctrine\RegistryInterface;
  */
 class CommentLikeRepository extends ServiceEntityRepository
 {
-    public function __construct(RegistryInterface $registry)
+    public function __construct(ManagerRegistry $registry)
     {
         parent::__construct($registry, CommentLike::class);
     }
--- src/Repository/StarRepository.php
+++ src/Repository/StarRepository.php
@@ -4,7 +4,7 @@ namespace App\Repository;
 
 use App\Entity\Star;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
-use Symfony\Bridge\Doctrine\RegistryInterface;
+use Doctrine\Persistence\ManagerRegistry;
 
 /**
  * @method Star|null find($id, $lockMode = null, $lockVersion = null)
@@ -14,7 +14,7 @@ use Symfony\Bridge\Doctrine\RegistryInterface;
  */
 class StarRepository extends ServiceEntityRepository
 {
-    public function __construct(RegistryInterface $registry)
+    public function __construct(ManagerRegistry $registry)
     {
         parent::__construct($registry, Star::class);
     }
